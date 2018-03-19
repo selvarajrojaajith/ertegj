cde4574c502fc91e41571148f5a68aeeb48682ed
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-19 16:36:23 +0530

    range.c
    good

---
+++ range.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+bool inRange(unsigned low, unsigned high)
+{
+    return (low <= x  && x <= high);
+}
+ 
+int main()
+{
+   printf("cout << "Yes\n":  cout  <<"No\n";
+   printf("cout << "Yes\n":  cout  <<"No\n";
+}
